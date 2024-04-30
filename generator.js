ed24c4a08c1a702cd9fb35aa0a36c84730f03964
rph <rhansen0127@gmail.com>
2024-04-30 18:31:50 -0500

    added questions for license and description of the project

--- generator.js
+++ generator.js
@@ -3,14 +3,41 @@ import fs from "fs";
 
 const questions = [
   {
-    type: input,
-    name: title,
+    type: "input",
+    name: "title",
     message: "What is the title of your project?",
     validate: (title) => {
       if (title) {
         return true;
       } else {
         console.log("Please enter a valid title for your project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license would you like to use for your project?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None", "Other"],
+    validate: (license) => {
+      if (license) {
+        return true;
+      } else {
+        console.log("Please select a valid license for your project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project.",
+    validate: (description) => {
+      if (description) {
+        return true;
+      } else {
+        console.log("Please provide a valid description of your project.");
         return false;
       }
     },
@@ -22,3 +49,5 @@ function init() {
     console.log(answers);
   });
 }
+
+init();
