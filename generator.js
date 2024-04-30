7c8553ab8dc11a65b70e9a6eb48cf4d0b51b0b63
rph <rhansen0127@gmail.com>
2024-04-30 18:17:21 -0500

    first question added.

--- generator.js
+++ generator.js
@@ -1,3 +1,24 @@
-const myConstant = "Hello";
-myConstant = "World";
-console.log(myConstant);
+import inquirer from "inquirer";
+import fs from "fs";
+
+const questions = [
+  {
+    type: input,
+    name: title,
+    message: "What is the title of your project?",
+    validate: (title) => {
+      if (title) {
+        return true;
+      } else {
+        console.log("Please enter a valid title for your project");
+        return false;
+      }
+    },
+  },
+];
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+  });
+}
